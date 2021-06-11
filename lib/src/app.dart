1697d8969d7dda377fb65c95ae7ef87e0020cdc3
Jorge Mollericon <jlmollericon@gmail.com>
2021-06-11 11:34:57 -0400

    :sparkles: add home page (+dir pages), and use this into app.dart

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 
+import 'package:counter/src/pages/home_page.dart';
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(context) {
     return MaterialApp(
       home: Center(
-        child: Text('Flutter'),
+        child: HomePage(),
       ),
     );
   }
