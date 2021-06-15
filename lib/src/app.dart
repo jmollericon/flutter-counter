eb5b5887a0a0075876403c700f3257accc9e71f6
Jorge Mollericon <jlmollericon@gmail.com>
2021-06-15 18:55:00 -0400

    :art: se quito el banner debug, en app.dart -> MaterialApp, debugShowCheckedModeBanner->false

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
-// import 'package:counter/src/pages/home_page.dart';
 import 'package:counter/src/pages/counter_page.dart';
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Center(
         child: CounterPage(),
       ),
